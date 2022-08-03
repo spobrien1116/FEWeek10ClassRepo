d832582506388cd62135011e52cd90387a87ab1e
spobrien1116 <87784429+spobrien1116@users.noreply.github.com>
2022-08-02 20:13:15 -0400

    End of class
    My attempt at doing the practice. More review of the videos, the slides, and the instructors posted example will be needed.

---
+++ practice10.js
@@ -0,0 +1,8 @@
+const newDiv = document.createElement('div');
+const div = document.div;
+div.append(newDiv);
+newDiv.innerText = "Hello World";
+
+function styleFont() {
+    newDiv.style.fontSize = '3rem'
+}
